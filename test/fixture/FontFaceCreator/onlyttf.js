441c7ecce11f967ba2f43f61d261d7ad119eda48
Raphael D. Pinheiro <raphaklaus@gmail.com>
2016-03-28 20:05:45 -0300

    Added FontFaceCreator test fixtures

--- test/fixture/FontFaceCreator/onlyttf.js
+++
@@ -1,9 +0,0 @@
-var onlyttf = '@font-face {\n' +
-'  font-family: \'Roboto\';\n' + 
-'  src: local(\'Roboto\'),\n' +
-'    url(\'fonts/Roboto.ttf\') format(\'truetype\');\n' +
-'  font-weight: normal;\n' +
-'  font-style: normal;\n' +
-'}\n';
-
-module.exports = onlyttf;
--- test/fixture/FontFaceCreator/ttfandwoff.js
+++
@@ -1,8 +0,0 @@
-module.exports = '@font-face {\n' +
-'  font-family: \'Roboto\';\n' + 
-'  src: local(\'Roboto\'),\n' +
-'    url(\'fonts/Roboto.woff\') format(\'woff\'),\n' +
-'    url(\'fonts/Roboto.ttf\') format(\'truetype\');\n' +
-'  font-weight: normal;\n' +
-'  font-style: normal;\n' +
-'}\n';
