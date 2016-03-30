122359904eb5671474d43b9161c9bfb0a2fc5193
Raphael D. Pinheiro <raphaklaus@gmail.com>
2016-03-30 00:18:12 -0300

    Added tests to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 
 gulp.task('default', function(){
-  return gulp.src('src/*')
+  return gulp.src(['src/*.js', 'test/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
