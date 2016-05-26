d198502e384692b5d2dfbd3d1075052d7b2e3ec0
Raphael D. Pinheiro <raphaklaus@gmail.com>
2016-05-26 16:20:58 -0300

    Renamed JSON orchastration library

--- test/JsonifyTest.js
+++ test/JsonifyTest.js
@@ -3,12 +3,12 @@
 const chai = require('chai'),
   assert = chai.assert,
   sinon = require('sinon'),
-  Json = require('../lib/Json.js'),
+  Jsonify = require('../lib/Jsonify.js'),
   Util = require('../lib/Util.js'),
   fs = require('fs');
 
-describe('Json methods requirements',()=>{
-  var json = new Json();
+describe('Jsonify methods requirements', () =>{
+  var jsonify = new Jsonify();
   var sandbox;
 
   beforeEach(() => {
@@ -20,33 +20,33 @@ describe('Json methods requirements',()=>{
   });
 
   it('save(): Should have a createWriteStream method', () => {
-    var writeFileSpy = sandbox.spy(fs,'createWriteStream');
-    json.addFont('Roboto-Regular', {
+    var writeFileSpy = sandbox.spy(fs, 'createWriteStream');
+    jsonify.addFont('Roboto-Regular', {
       'family': 'roboto',
-      'format': ['.woff2','.woff','.eot']
+      'format': ['.woff2', '.woff', '.eot']
     });
-    json.save();
+    jsonify.save();
     sinon.assert.calledOnce(writeFileSpy);
   });
 
   it('fileExists(): Should have a statSync method', () => {
     var statSyncSpy = sandbox.spy(fs, 'statSync');
 
-    json.fileExists();
+    jsonify.fileExists();
     sinon.assert.calledOnce(statSyncSpy);
   });
 
   it('readFile(): Should have a readFileSync method', () => {
     var readFileSpy = sandbox.spy(fs, 'readFileSync');
 
-    json.readFile();
-    fs.unlinkSync(json.file);
+    jsonify.readFile();
+    fs.unlinkSync(jsonify.file);
     sinon.assert.calledOnce(readFileSpy);
   });
 });
 
-describe('Json rules',()=>{
-  var json = new Json();
+describe('Jsonify rules', ()=> {
+  var jsonify = new Jsonify();
 
   beforeEach(() => {
     this.roboto = {
@@ -62,11 +62,11 @@ describe('Json rules',()=>{
     this.opensans = {
       'Opensans-Regular': {
         'family': 'opensans',
-        'format': ['.woff2','.woff']
+        'format': ['.woff2', '.woff']
       },
       'Opensans-Light': {
         'family': 'opensans',
-        'format': ['.woff2','.woff']
+        'format': ['.woff2', '.woff']
       }
     };
     this.nexa = {
@@ -88,10 +88,10 @@ describe('Json rules',()=>{
   });
 
   it('addFont(): Should add Roboto-Light font', ()=> {
-    return Util.loadFixture('test/fixture/Json/robotoLight.json')
+    return Util.loadFixture('test/fixture/Jsonify/robotoLight.json')
       .then((data) => {
-        json.addFont('Roboto-Light',this.roboto['Roboto-Light']);
-        assert.deepEqual(json.json,JSON.parse(data));
+        jsonify.addFont('Roboto-Light', this.roboto['Roboto-Light']);
+        assert.deepEqual(jsonify.json, JSON.parse(data));
       }, (error) => {
         throw error;
       });
@@ -99,72 +99,74 @@ describe('Json rules',()=>{
 
   it('addFont(): Should throw an Error', ()=> {
     assert.throws(
-      json.addFont.bind(null, 'Roboto-Light',this.roboto['Roboto-Light'])
+      jsonify.addFont.bind(null, 'Roboto-Light', this.roboto['Roboto-Light'])
     );
   });
 
-  it('multiple methods: Should add some fonts and remove entire roboto tree', ()=> {
-    return Util.loadFixture('test/fixture/Json/removeFamily.json')
-      .then((data) => {
-        json.addFont('Opensans-Regular',this.opensans['Opensans-Regular'])
-          .addSystemFont('Verdana-Regular',this.verdana['Verdana-Regular'])
-          .removeFontFamily('roboto');
-        assert.deepEqual(json.json,JSON.parse(data));
-      }, (error) => {
-        throw error;
-      });
-  });
+  it('multiple methods: Should add some fonts and remove entire roboto tree',
+    () => {
+      return Util.loadFixture('test/fixture/Jsonify/removeFamily.json')
+        .then((data) => {
+          jsonify.addFont('Opensans-Regular', this.opensans['Opensans-Regular'])
+            .addSystemFont('Verdana-Regular', this.verdana['Verdana-Regular'])
+            .removeFontFamily('roboto');
+          assert.deepEqual(jsonify.json, JSON.parse(data));
+        }, (error) => {
+          throw error;
+        });
+    });
 
   it('changeOutput(): Should change fonts output', ()=> {
-    return Util.loadFixture('test/fixture/Json/output.json')
+    return Util.loadFixture('test/fixture/Jsonify/output.json')
       .then((data) => {
-        json.changeOutput(this.output['output']);
-        assert.deepEqual(json.json,JSON.parse(data));
+        jsonify.changeOutput(this.output['output']);
+        assert.deepEqual(jsonify.json, JSON.parse(data));
       }, (error) => {
         throw error;
       });
   });
 
   it('get(): Should get the output property', ()=> {
-    return Util.loadFixture('test/fixture/Json/get.json')
+    return Util.loadFixture('test/fixture/Jsonify/get.json')
       .then((data) => {
-        var output = json.get('output');
+        var output = jsonify.get('output');
         var parsed = JSON.parse(data);
-        assert.deepEqual(output,parsed.output);
+        assert.deepEqual(output, parsed.output);
       }, (error) => {
         throw error;
       });
   });
 
   it('getFontByName(): Should return the Opensans-Regular font', ()=> {
-    return Util.loadFixture('test/fixture/Json/get.json')
+    return Util.loadFixture('test/fixture/Jsonify/get.json')
       .then((data) => {
-        var font = json.getFontByName('Opensans-Regular');
+        var font = jsonify.getFontByName('Opensans-Regular');
         var parsed = JSON.parse(data);
-        assert.deepEqual(font,parsed.fonts['Opensans-Regular']);
+        assert.deepEqual(font, parsed.fonts['Opensans-Regular']);
       }, (error) => {
         throw error;
       });
   });
 
   it('getFontsByFamily(): Should return the entire opensans tree', ()=> {
-    return Util.loadFixture('test/fixture/Json/get.json')
+    return Util.loadFixture('test/fixture/Jsonify/get.json')
       .then((data) => {
-        json.addFont('Opensans-Light',this.opensans['Opensans-Light']);
-        var fonts = json.getFontsByFamily('opensans');
+        jsonify.addFont('Opensans-Light', this.opensans['Opensans-Light']);
+        var fonts = jsonify.getFontsByFamily('opensans');
         var parsed = JSON.parse(data);
-        assert.deepEqual(fonts,parsed.fonts);
+        assert.deepEqual(fonts, parsed.fonts);
       }, (error) => {
         throw error;
       });
   });
 
-  it('removeFont(), removeSystemFont(): Should remove Opensans-Regular and Verdana-Regular fonts', ()=> {
-    return Util.loadFixture('test/fixture/Json/removeFonts.json')
+  it('removeFont(), removeSystemFont(): Should remove Opensans-Regular ' +
+    'and Verdana-Regular fonts', ()=> {
+    return Util.loadFixture('test/fixture/Jsonify/removeFonts.json')
       .then((data) => {
-        json.removeFont('Opensans-Light')
+        jsonify.removeFont('Opensans-Light')
           .removeSystemFont('Verdana-Regular');
-        assert.deepEqual(json.json,JSON.parse(data));
+        assert.deepEqual(jsonify.json, JSON.parse(data));
       }, (error) => {
         throw error;
       });
