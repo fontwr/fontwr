ca0f907e3d36283612ecbd9e1da5c2a752c6728b
Raphael D. Pinheiro <raphaklaus@gmail.com>
2016-05-26 16:29:06 -0300

    Added lib/ and gulpfile to eslint task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 
-gulp.task('default', function(){
-  return gulp.src(['src/*.js', 'test/*.js'])
+gulp.task('default', function() {
+  return gulp.src(['src/*.js', 'test/*.js', 'lib/*.js', './gulpfile.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
